278f0b9b081e0264b5e609371120e9062beaea30
Marvel Johnson <marvjohnson@twilio.com>
2022-11-07 17:32:26 -0800

    Updated objectiveValidation
    -Added check for single and double quotes in TQ_GIT_EXE, which throws a new exception. Users were directed to add quotes to their paths until a
    fix was deployed. This ensures these users know how to correct their variable once this goes live
    -Added comment

--- scripts/objectiveValidation.js
+++ scripts/objectiveValidation.js
@@ -10,6 +10,18 @@ async function execGitWithArgs(helper, argString, execOptions = {}) {
     );
   }
 
+  // There was a bug introduced in the TQ 3.10.0 release that resulted in "exec" calls failing if the user's
+  // Git path had spaces. During that time, we recommended player's add quotes to their environment
+  // variable to circumvent the issue. Since we're doing that ourselves now, we want to be sure player's aren't
+  if (
+    (TQ_GIT_EXE.startsWith('"') && TQ_GIT_EXE.endsWith('"')) ||
+    (TQ_GIT_EXE.startsWith("'") && TQ_GIT_EXE.endsWith("'"))
+  ) {
+    throw new Error(
+      "TwilioQuest no longer requires you to surround your git executable path with quotation marks if it contains spaces. Go into Settings -> Variables and remove any quotes around the TQ_GIT_EXE variable!"
+    );
+  }
+
   return exec(`"${TQ_GIT_EXE}" ${argString}`, execOptions);
 }
 
